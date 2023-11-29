80b062c91e8b346724470e31873cd9362b889a90
tim9898 <152410300+tim9898@users.noreply.github.com>
2023-11-29 16:44:53 +0100

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,1 +1,25 @@
+from flask import Flask, render_template, request, jsonify
 
+app = Flask(__name__)
+
+# Beispiel-Daten
+mobility_data = {
+    "locations": ["Ort A", "Ort B", "Ort C"],
+    "fahrzeuge": ["Auto", "Fahrrad", "Elektroroller"]
+}
+
+# Routen
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/locations', methods=['GET'])
+def get_locations():
+    return jsonify(mobility_data['locations'])
+
+@app.route('/vehicles', methods=['GET'])
+def get_vehicles():
+    return jsonify(mobility_data['fahrzeuge'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
